f7982562b1a05319259e744511b0556a0b4d2009
jhonsegurag <jhonsegurag@gmail.com>
2014-04-22 00:22:44 -0500

    Traduccion Español

--- app/Config/core.php
+++ app/Config/core.php
@@ -33,6 +33,8 @@
  */
 	Configure::write('debug', 2);
 
+	
+	Configure::write('Config.language', 'spa');
 /**
  * Configure the Error handler used to handle errors for your application. By default
  * ErrorHandler::handleError() is used. It will display errors using Debugger, when debug > 0
